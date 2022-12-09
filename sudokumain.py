53b9e27d72dceade7db6dd0da4f12c2817f1e2f3
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-12-09 20:08:27 +0530

    created Tile class

--- sudokumain.py
+++ sudokumain.py
@@ -137,4 +137,41 @@ class Board:
 
 
 
+class Tile:
+
+
+    def __init__(
+        self,
+        value,
+        window,
+        x1,
+        y1,
+        ):
+        self.value = value
+        self.window = window
+        self.rect = pygame.Rect(x1, y1, 60, 60)  
+        self.selected = False
+        self.correct = False
+        self.incorrect = False
+
+    def draw(self, color, thickness):
+
+        pygame.draw.rect(self.window, color, self.rect, thickness)
+
+    def display(
+        self,
+        value,
+        position,
+        color,
+        ):
+
+        font = pygame.font.SysFont('lato', 45)
+        text = font.render(str(value), True, color)
+        self.window.blit(text, position)
+
+    def clicked(self, mousePos):
+
+        if self.rect.collidepoint(mousePos):
+            self.selected = True
+        return self.selected
 pygame.quit()
